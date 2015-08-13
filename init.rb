14ec2048bcd1639072c9bbb5e09ba835b6827c76
shawndibble <shawndibble@gmail.com>
2015-08-13 15:53:14 -0500

    Allow projects to enable/disable as needed
    Makes the plugin a module plugin. Thus managers can go in and enable/disable this functionality as needed.

--- init.rb
+++ init.rb
@@ -7,6 +7,8 @@ Redmine::Plugin.register :msproject_import do
   author_url 'https://github.com/braini75'
   
   settings :default => {'tracker_default' => 2}, :partial => 'settings/msproject_import'
-  permission :msproject_import, { :msproj_imp => [:upload, :import, :analyze, :import_results]}
+  project_module msproject_import do
+    permission :msproject_import, { :msproj_imp => [:upload, :import, :analyze, :import_results]}
+  end
   menu :project_menu, :msproject_import, { :controller => 'msproj_imp', :action => 'upload' }, :caption => :menu_caption, :after => :settings, :param => :project_id  
 end
